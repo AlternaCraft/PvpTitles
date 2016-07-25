3d7d82833b32a47e6bb5de4fd8aa1fed3507c3c5
esejuli94 <juli_sesena@hotmail.com>
2016-07-25 04:55:26 +0200

    Auto deploy gh-pages

--- .utility/deploy-javadoc.sh
+++ .utility/deploy-javadoc.sh
@@ -26,6 +26,6 @@ git rm -rf ./javadoc
 cp -Rf $HOME/javadoc-latest ./javadoc
 git add -f .
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to $TARGET_BRANCH"
-git push -fq esejuli94 $TARGET_BRANCH > /dev/null
+git push -fq origin $TARGET_BRANCH > /dev/null
 
-echo -e "Published Javadoc to gh-pages.\n"
+echo "Published Javadoc to gh-pages.\n"
