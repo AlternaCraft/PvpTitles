48f70bac2b652a6830d9dada3cb7947834f69572
esejuli94 <juli_sesena@hotmail.com>
2016-07-26 22:21:40 +0200

    Auto deploy gh-pages

--- .utility/deploy-javadoc.sh
+++ .utility/deploy-javadoc.sh
@@ -12,12 +12,13 @@ fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SSH_REPO=$(echo $REPO | sed -e "s/https:\/\/github.com\//git@github.com:/g")
 SHA=`git rev-parse --verify HEAD`
 
 echo "Publishing javadoc...\n"
 
 cp -R target/site/apidocs $HOME/javadoc-latest
+cp .utility/deploy_key.enc $HOME/deploy_key.enc
 
 cd $HOME
 
@@ -41,7 +42,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $HOME/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval "$(ssh-agent -s)"
 ssh-add deploy_key
