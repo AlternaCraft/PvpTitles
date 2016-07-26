8dc3651a8752f1529099e250b52f366287706306
esejuli94 <juli_sesena@hotmail.com>
2016-07-27 00:39:06 +0200

    Auto deploy gh-pages - Last changes

--- .utility/deploy-javadoc.sh
+++ .utility/deploy-javadoc.sh
@@ -12,19 +12,16 @@ fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
-SSH_REPO=$(echo $REPO | sed -e "s/https:\/\/github.com\//git@github.com:/g")
-SHA=`git rev-parse --verify HEAD`
 
 echo "Publishing javadoc...\n"
 
 cp -R target/site/apidocs $HOME/javadoc-latest
-cp .utility/deploy_key.enc $HOME/deploy_key.enc
 
 cd $HOME
 
 git clone --quiet --branch=$TARGET_BRANCH $REPO $TARGET_BRANCH
 
-git config --global user.name "Travis CI"
+git config --global user.name "$COMMIT_AUTHOR_NAME"
 git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 
 cd gh-pages
@@ -37,18 +34,7 @@ cp -Rf $HOME/javadoc-latest ./javadoc
 git add .
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to $TARGET_BRANCH"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY="\$${ENCRYPTED_KEY_VAR}"
-ENCRYPTED_IV="\$${ENCRYPTED_IV_VAR}"
-#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $HOME/deploy_key.enc -out deploy_key -d
-openssl aes-256-cbc -K $encrypted_4b7ad6b3cb7b_key -iv $encrypted_4b7ad6b3cb7b_iv -in $HOME/deploy_key.enc -out $HOME/deploy_key -d
-chmod 600 $HOME/deploy_key
-eval "$(ssh-agent -s)"
-ssh-add $HOME/deploy_key
-
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push origin $TARGET_BRANCH
 
 echo "Published Javadoc to gh-pages."
