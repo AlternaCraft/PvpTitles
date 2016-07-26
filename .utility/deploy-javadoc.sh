ffbc74a93ded0254f7818cc6b6fb3fc73665b780
esejuli94 <juli_sesena@hotmail.com>
2016-07-27 00:11:23 +0200

    Auto deploy gh-pages

--- .utility/deploy-javadoc.sh
+++ .utility/deploy-javadoc.sh
@@ -42,7 +42,8 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY="\$${ENCRYPTED_KEY_VAR}"
 ENCRYPTED_IV="\$${ENCRYPTED_IV_VAR}"
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $HOME/deploy_key.enc -out deploy_key -d
+#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $HOME/deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $encrypted_0a4b7ad6b3cb7b_key -iv $encrypted_0a4b7ad6b3cb7b_iv -in $HOME/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval "$(ssh-agent -s)"
 ssh-add deploy_key
