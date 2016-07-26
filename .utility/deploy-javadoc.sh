5f505bbc1d94f3bd8b0e1321f12a7699dccc83ff
esejuli94 <juli_sesena@hotmail.com>
2016-07-26 22:09:50 +0200

    Auto deploy gh-pages

--- .utility/deploy-javadoc.sh
+++ .utility/deploy-javadoc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="2.x"
 TARGET_BRANCH="gh-pages"
@@ -10,28 +10,43 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
 echo "Publishing javadoc...\n"
 
 cp -R target/site/apidocs $HOME/javadoc-latest
 
-# RELEASE_VERSION=`mvn help:evaluate -Dartifact=com.alternacraft:PvpTitles:jar -Dexpression=project.version`
-
 cd $HOME
+
+git clone --quiet --branch=$TARGET_BRANCH $REPO $TARGET_BRANCH
 
 git config --global user.name "Travis CI"
 git config --global user.email "$COMMIT_AUTHOR_EMAIL"
-git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/AlternaCraft/Pvptitles $TARGET_BRANCH > /dev/null
-
-#echo $RELEASE_VERSION
 
 cd gh-pages
 
-#git rm -rf --ignore-unmatch ./javadoc/$RELEASE_VERSION
+# Save the latest javadoc
 git rm -rf --ignore-unmatch ./javadoc
-#cp -Rf $HOME/javadoc-latest ./javadoc/$RELEASE_VERSION
 cp -Rf $HOME/javadoc-latest ./javadoc
-git add -f .
+
+# Add and commit new files
+git add .
 git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to $TARGET_BRANCH"
-git push -fq origin $TARGET_BRANCH > /dev/null
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval "$(ssh-agent -s)"
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
 
 echo "Published Javadoc to gh-pages."
