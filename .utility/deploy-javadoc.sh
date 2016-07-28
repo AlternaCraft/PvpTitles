11b9c6ace1f2c6b21c5f0fdf8e59bbefc8ff8a9b
esejuli94 <juli_sesena@hotmail.com>
2016-07-28 20:29:59 +0200

    Auto deploy gh-pages

--- .utility/deploy-javadoc.sh
+++ .utility/deploy-javadoc.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="2.x"
 TARGET_BRANCH="gh-pages"
 
 # Pull requests, forks and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_REPO_SLUG" != "AlternaCraft/PvpTitles" -o "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ "$TRAVIS_REPO_SLUG" != "AlternaCraft/*" -o "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
     exit 0
 fi
