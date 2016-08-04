0a3f99de7a40f8517f5c98b828352a26c1de493f
esejuli94 <juli_sesena@hotmail.com>
2016-08-04 23:28:34 +0200

    Fixed

--- app/shared/jd/JDDirective.js
+++ app/shared/jd/JDDirective.js
@@ -2,7 +2,7 @@ app.directive('jd', function() {
     return {
         restrict: 'E',
         link: function(scope, element, attrs) {
-            element.replaceWith('<object class="javadoc" type="text/html" data="/javadoc/' + attrs.src + '/"></object>');
+            element.replaceWith('<object class="javadoc" type="text/html" data="/PvpTitles/javadoc/' + attrs.src + '/"></object>');
         }
     };
 });
