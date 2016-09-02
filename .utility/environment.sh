1b8cd316a01c4261421175c2d2358f3c16fc6c2f
esejuli94 <juli_sesena@hotmail.com>
2016-09-02 19:18:04 +0200

    Travis changes

--- .utility/environment.sh
+++ .utility/environment.sh
@@ -2,4 +2,13 @@
 
 echo "<settings><servers><server><id>bintray</id><username>\${BINTRAY_USER}</username><password>\${BINTRAY_PASS}</password></server></servers></settings>" > ~/settings.xml
 
-[[ ! -z $TRAVIS_TAG ]] && bash .utility/parse-dependencies.sh && bash .utility/deploy-pages.sh || mvn deploy --settings ~/settings.xml
+if [[ ! -z $TRAVIS_TAG ]]; then
+  VERSION=`ls target/PvpTitles-*.jar | sed 's/target\/PvpTitles-//;s/.jar//;'`  
+  
+  # Skipping snapshots
+  if [[ ! $VERSION =~ "SNAPSHOT" ]]; then
+    bash .utility/parse-dependencies.sh && bash .utility/deploy-pages.sh
+  fi
+else
+  mvn deploy --settings ~/settings.xml
+fi
