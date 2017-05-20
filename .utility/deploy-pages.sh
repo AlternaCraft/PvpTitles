4105b44d363b9911fb16cf04984b30cc990a252c
esejuli94 <juli_sesena@hotmail.com>
2017-05-20 21:27:39 +0200

    Testing travis autodeploy.

--- .utility/deploy-pages.sh
+++ .utility/deploy-pages.sh
@@ -3,12 +3,10 @@
 # Save some useful information
 TARGET_BRANCH="gh-pages"
 
-URL=`git config remote.origin.url`
-REPO=$(echo $URL | sed -e "s/https:\/\//https:\/\/${GH_TOKEN}@/g")
+REPO=`git config remote.origin.url`
+#REPO=$(echo $URL | sed -e "s/https:\/\//https:\/\/${GH_TOKEN}@/g")
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 VERSION=`ls target/PvpTitles-*.jar | sed 's/target\/PvpTitles-//;s/.jar//;'`
-
-COMMIT_AUTHOR_NAME="Travis CI"
-COMMIT_AUTHOR_EMAIL="esejuli94@gmail.com"
 
 echo "Getting dependencies file..."
 
@@ -24,10 +22,11 @@ cd $HOME
 
 # Get repository
 git clone --quiet --branch=$TARGET_BRANCH $REPO gh-pages
-git config --global user.name "$COMMIT_AUTHOR_NAME"
-git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 
 cd gh-pages
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # Save dependencies json
 git rm -rf --ignore-unmatch ./dependencies/$VERSION
@@ -39,10 +38,22 @@ git rm -rf --ignore-unmatch ./javadoc/$VERSION
 cp -a $HOME/javadoc-latest/. ./javadoc/$VERSION
 
 # Add and commit new files
-git add .
+git add -A .
 git commit -m "Latest components on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to $TARGET_BRANCH"
 
+cd $HOME
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in .utility/deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
 # Now that we're all set up, we can push.
-git push origin $TARGET_BRANCH
+git push $SSH_REPO $TARGET_BRANCH
 
 echo "Published Javadoc and dependencies to gh-pages."
