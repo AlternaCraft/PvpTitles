28df4be48a489e2c229492d7f2f464159fe167e5
esejuli94 <juli_sesena@hotmail.com>
2017-05-20 22:54:11 +0200

    Testing travis autodeploy.

--- .utility/environment.sh
+++ .utility/environment.sh
@@ -4,6 +4,7 @@ echo "<settings><servers><server><id>bintray</id><username>\${BINTRAY_USER}</use
 
 if [[ ! -z $TRAVIS_TAG ]]; then
   ARTIFACT_ID=`echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:artifactId/text()' | xmllint --shell pom.xml | grep -v /`
+  echo $ARTIFACT_ID
   VERSION=`ls target/$ARTIFACT_ID-*.jar | sed 's/target\/$ARTIFACT_ID-//;s/.jar//;'`  
   
   # Skipping snapshots
