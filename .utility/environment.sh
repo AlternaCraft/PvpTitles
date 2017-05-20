8215d7e3ca5af27a851dd39680a240807b11fb56
esejuli94 <juli_sesena@hotmail.com>
2017-05-20 23:18:21 +0200

    Testing travis autodeploy.

--- .utility/environment.sh
+++ .utility/environment.sh
@@ -3,9 +3,7 @@
 echo "<settings><servers><server><id>bintray</id><username>\${BINTRAY_USER}</username><password>\${BINTRAY_PASS}</password></server></servers></settings>" > ~/settings.xml
 
 if [[ ! -z $TRAVIS_TAG ]]; then
-  ARTIFACT_ID=`echo -e 'setns x=http://maven.apache.org/POM/4.0.0\ncat /x:project/x:artifactId/text()' | xmllint --shell pom.xml | grep -v /`
-  echo $ARTIFACT_ID
-  VERSION=`ls target/$ARTIFACT_ID-*.jar | sed 's/target\/$ARTIFACT_ID-//;s/.jar//;'`  
+  VERSION=`xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml`  
   
   # Skipping snapshots
   if [[ ! $VERSION =~ "SNAPSHOT" ]]; then
